7e3628237e46982c27c5878902f6be7455ffa6bf
specture724 <specture724@gmail.com>
2026-08-10 12:08:31 +0800

    feat: deducing DBO instead of voting

--- vllm/v1/worker/gpu/model_runner.py
+++ vllm/v1/worker/gpu/model_runner.py
@@ -1313,7 +1313,7 @@ class GPUModelRunner(LoRAModelRunnerMixin):
             self.dp_rank,
             need_eager=is_profile or skip_compiled,
             num_active_loras=num_active_loras,
-            ubatch_runner=None if dummy_run else self.ubatch_runner,
+            ubatch_runner=None if skip_attn_for_dummy_run else self.ubatch_runner,
         )
 
         if batch_desc.num_tokens == 0:
